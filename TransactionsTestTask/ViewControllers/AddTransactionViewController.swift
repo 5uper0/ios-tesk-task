f5dc8803591c55079278c962d756aa587345cb1c
Oleh Veheria <oleh@veheria.tech>
2025-01-06 18:25:55 +0100

    Refine project structure for App and Configuration files

---
+++ TransactionsTestTask/ViewControllers/AddTransactionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddTransactionViewController.swift
+//  TransactionsTestTask
+//
+//  Created by Oleh Veheria on 06.01.2025.
+//
+
+import UIKit
+
+class AddTransactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
